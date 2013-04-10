d2a593c37cf8e0ed8af9c1067d2105e81bb9b50b
Craig Hurley <craig@thehurley.com>
2013-04-10 17:53:15 +1200

    Added code to select all text before running xmllint, then clear selection and place the cursor at the start of the document.

--- XmlLintCommand.py
+++ XmlLintCommand.py
@@ -7,17 +7,27 @@ class XmlLintCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         command = "XMLLINT_INDENT=$'    ' xmllint --format --encode utf-8 -"
 
+        # select all text before begining
+        self.view.sel().add(sublime.Region(0, self.view.size()))
+
+        # try running xmllint on the select text
         p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
         result, err = p.communicate(self.view.substr(self.view.sel()[0]).encode('utf-8'))
 
         if err != "":
-            self.view.set_status('xmllint', "xmllint: " + err)
+            # there was an error, report it
+            sublime.status_message("xmllint: " + err)
             sublime.set_timeout(self.clear, 10000)
 
         else:
+            # xmllint ran successfully, so replace the selected text
             self.view.replace(edit, self.view.sel()[0], result.decode('utf-8'))
             sublime.set_timeout(self.clear, 0)
-            sublime.status_message("xmllint success")
+            # report success
+            sublime.status_message("xmllint: success")
+            # clear the selection and place the cursor at the start of the document
+            self.view.sel().clear()
+            self.view.sel().add(sublime.Region(0, 0))
 
     def clear(self):
         self.view.erase_status('xmllint')
